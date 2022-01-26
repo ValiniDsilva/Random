c23c6ed43bf2599bbed35779a76bd6efbbef559a
Valini Dsilva <86795746+ValiniDsilva@users.noreply.github.com>
2022-01-26 17:23:48 +0530

    Update Untitled-3.py

--- Untitled-3.py
+++ Untitled-3.py
@@ -10,15 +10,3 @@ u = str(input("Enter name: "))
 hi(u)
 
 
-'''
-def smaller_num(x, y): ## Can be rephrased to  def smaller_num(x, y):
-    if x > y:          ##                          if x > y:
-        number = y     ##                              return y
-    else:              ##                          else:
-        number = x     ##                              return x
-    return number
-
-x = input("Enter first number:-")
-y = input("Enter second number:-")
-result = smaller_num(x, y)
-print("The smaller number between " +  str(x) + " and " + str(y) + " is " + str(result))'''
